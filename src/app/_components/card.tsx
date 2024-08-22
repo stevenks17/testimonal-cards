a481a687e4ed365da72c90cfb45f9e9c58dc542a
steven <stevenks17@gmail.com>
2024-08-22 14:58:03 -0400

    moved font style to parent to trickle and added specific tw class leading for line heights of text

--- src/app/_components/card.tsx
+++ src/app/_components/card.tsx
@@ -7,25 +7,25 @@ interface CardProps {
 
 export default function Card({ user }: CardProps) {
   return (
-    <div className="flex w-[340px] flex-col gap-4 rounded-lg bg-white p-6">
+    <div className="flex w-[340px] flex-col gap-4 rounded-lg bg-white p-6 font-sans shadow-sm">
       <div className="flex items-center gap-4 self-stretch">
         <Image
-          className="h-12 w-12 object-cover"
+          className="h-12 w-12 rounded-full object-cover"
           alt={`${user.name}'s profile picture`}
           src={user.image ?? "/profile-thumbnail.png"}
           width={48}
           height={48}
         ></Image>
         <div className="flex grow flex-col gap-px">
-          <span className="text-justify font-sans text-lg font-semibold text-neutral-900">
+          <span className="font-semibold leading-7 text-neutral-900">
             {user.name}
           </span>
-          <span className="font-sans text-sm font-normal text-neutral-600">
+          <span className="text-sm font-normal leading-5 text-neutral-600">
             @{user.username}
           </span>
         </div>
       </div>
-      <span className="font-sans text-base font-normal text-neutral-600">
+      <span className="text-base font-normal leading-6 text-neutral-600">
         {user.description}
       </span>
     </div>
